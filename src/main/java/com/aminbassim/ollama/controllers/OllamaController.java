54333184285e3fa4a882fdc5b65a8e5b6c3ecd48
ZeroPii <info@zeropixel.es>
2025-05-29 15:57:19 +0200

    first commit

---
+++ src/main/java/com/aminbassim/ollama/controllers/OllamaController.java
@@ -0,0 +1,21 @@
+package com.aminbassim.ollama.controllers;
+
+import com.aminbassim.ollama.services.OllamaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/")
+public class OllamaController {
+
+    @Autowired
+    private OllamaService ollamaService;
+
+    @GetMapping("generate")
+    public String generate(@RequestParam(value = "promptMessage") String promptMessage) {
+        return ollamaService.generateResult(promptMessage);
+    }
+}
